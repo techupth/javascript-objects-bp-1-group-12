e93a0a732352729c9f208a4fe0368efd1f61f933
James <Natasan2121@gmail.com>
2023-09-29 14:41:58 +0700

    complete objects assginment

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,14 @@
 // Exercise #1: Food Order
 
-// Start coding here
+// Start code here
+const foodOrder = {
+  orderNumber : "A0234",
+  address : "Bangkok",
+  peymentType : "Cash",
+  totalPrice : 4500,
+  restaurantName : "MK"
+}
+
+foodOrder["totalPrice"] = 5000
+foodOrder["peymentType"] = "Credit Card"
+console.log(foodOrder)
